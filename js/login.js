e5752d603a9a1455c0addd921d072559efba3753
brunolarre14 <brunoemilianolarre@gmail.com>
2023-08-12 12:05:45 -0300

    fix login form

--- js/login.js
+++ js/login.js
@@ -1,9 +1,9 @@
 function validar() {
-    let mail = document.getElementById("email").value;
+    let user = document.getElementById("user").value;
     let password = document.getElementById("password").value;
     let recordar = document.getElementById("recordarme").checked;
 
-    if ((mail.length) > 0 && (password.length > 0) && (recordar === true)) {
+    if ((user.length) > 0 && (password.length > 0) && (recordar === true)) {
         window.location.href = "index.html";
 
     } else {
