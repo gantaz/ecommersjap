b4b703ce51c1e3282f6e5695a13ecb2601f48805
brunolarre14 <brunoemilianolarre@gmail.com>
2023-08-12 11:18:02 -0300

    Agrego logica a login.js

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function validar() {
+    let mail = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let recordar = document.getElementById("recordarme").checked;
+
+    if ((mail.length) > 0 && (password.length > 0) && (recordar === true)) {
+        window.location.href = "index.html";
+
+    } else {
+        alert("Datos incorrectos")
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+const enviar = document.getElementById("submitBtn");
+
+enviar.addEventListener("click", validar);
+
+})
