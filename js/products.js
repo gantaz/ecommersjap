4d0803538c8b923591927c482acdc54424831f19
Gabriel Núñez <gabrielnz23.gne@gmail.com>
2023-08-22 22:40:09 -0300

    Ahora se muestran las categorias marcadas por el id en productos

--- js/products.js
+++ js/products.js
@@ -1,9 +1,9 @@
-const PRODUCTS_DATA = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const PRODUCTS_DATA = "https://japceibal.github.io/emercado-api/cats_products/"+ localStorage.getItem("catID") + ".json";
 
-const autos = document.getElementById("autos");
+const categoria = document.getElementById("categoria");
 function showData(dataArray) {
   for (const item of dataArray) {
-    autos.innerHTML += `<div onclick="setCatID(${item.id})" class="list-group-item list-group-item-action cursor-active">
+    categoria.innerHTML += `<div onclick="setCatID(${item.id})" class="list-group-item list-group-item-action cursor-active">
         <div class="row">
             <div class="col-3">
                 <img src="${item.image}" alt="${item.description}" class="img-thumbnail">
