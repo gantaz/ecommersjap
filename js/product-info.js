51da5fcf289e177825985f253411ff61610c71e2
rigantidiego <rigantidiego@gmail.com>
2023-09-08 14:53:36 -0300

    desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -67,3 +67,46 @@ fetch(PRODUCTS_INFO)
     });
 
 
+// desafiate
+// variables e ids
+var sendBoton = document.getElementById('sendComentario');
+var comentarioTextArea = document.getElementById('ingresarComentario');
+var puntuacionSelect = document.getElementById('puntos');
+var comentariosContainer = document.getElementById('comentarios');
+
+// boton
+sendBoton.addEventListener('click', function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe realmente
+  
+    // values comment y puntuación
+    var comentario = comentarioTextArea.value;
+    var puntuacion = puntuacionSelect.value;
+    var usuario = localStorage.getItem("user") || sessionStorage.getItem("user"); // agarra el usuario del local storage o session
+  
+    // Fecha convertida a formato legible string
+    var fecha = new Date().toLocaleString();
+  
+    // crear div para el comment nuevo y su formato
+    var nuevoComentario = document.createElement('div');
+    nuevoComentario.innerHTML = `
+        <div>
+            <b>${usuario}:</b> ${comentario} <br>
+            ${fecha}
+        </div>
+    `;
+  
+    // Bootstrap estrellas
+    for (let j = 1; j <= 5; j++) {
+        if (j <= puntuacion) {
+            nuevoComentario.innerHTML += `<span class="fa fa-star checked"></span>`;
+        } else {
+            nuevoComentario.innerHTML += `<span class="fa fa-star"></span>`;
+        }
+    }
+  
+    // agregar comentario fake al container
+    comentariosContainer.appendChild(nuevoComentario);
+  
+    // deja vacio el textarea para escribir otro comment
+    comentarioTextArea.value = '';
+});
